5a0a69d5d608397dafae8fe2f9728fbc7b300b06
Haotong Fu <112030206+MarkFu0213@users.noreply.github.com>
2024-11-24 01:10:40 -0800

    Added tests for feature 2 and 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,10 +30,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    testOptions {
+        animationsDisabled = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -41,7 +44,24 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-analytics")
-    testImplementation(libs.junit)
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+
     androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("org.hamcrest:hamcrest:2.2")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1") {
+        exclude(group = "org.checkerframework", module = "checker")
+    }
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("org.hamcrest:hamcrest:2.2")
+        force("junit:junit:4.13.2")
+    }
 }
